aad5734d68f87e9840fd93f20d996c643cfb356e
LenouarMiloud <lenouarmiloud@gmail.com>
2024-08-14 15:07:32 -0700

    add functions to scan the qr code

--- app/src/main/java/com/fsociety/qrcodeapp/MainActivity.kt
+++ app/src/main/java/com/fsociety/qrcodeapp/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.fsociety.qrcodeapp
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
@@ -12,9 +21,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.fsociety.qrcodeapp.ui.theme.QRCodeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,5 +47,54 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(){
-
+    var code by remember {
+        mutableStateOf("")
+    }
+    val context = LocalContext.current
+    val cameraProviderFuture = remember {
+        ProcessCameraProvider.getInstance(context)
+    }
+    var hasCameraPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            )== PackageManager.PERMISSION_GRANTED
+        )
+    }
+    var launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { granted ->
+            hasCameraPermission = granted
+        }
+    )
+    LaunchedEffect(key1 = true) {
+        launcher.launch(Manifest.permission.CAMERA)
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        AndroidView(factory = {context ->
+            val previewView = PreviewView(context)
+            val preview = Preview.Builder().build()
+            val selector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+            preview.setSurfaceProvider(previewView.surfaceProvider)
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setTargetResolution(
+                    Size(
+                        previewView.width,
+                        previewView.height
+                        ))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build()
+            imageAnalysis.setAnalyzer(
+                ContextCompat.getMainExecutor(context),
+                QRCodeAnalyzer{result->
+                    code = result
+                }
+            )
+        })
+    }
 }
--- app/src/main/java/com/fsociety/qrcodeapp/QRCodeAnalyzer.kt
+++ app/src/main/java/com/fsociety/qrcodeapp/QRCodeAnalyzer.kt
@@ -3,7 +3,10 @@ package com.fsociety.qrcodeapp
 import android.graphics.ImageFormat
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.zxing.BarcodeFormat
 import com.google.zxing.BinaryBitmap
+import com.google.zxing.DecodeHintType
+import com.google.zxing.MultiFormatReader
 import com.google.zxing.PlanarYUVLuminanceSource
 import com.google.zxing.common.HybridBinarizer
 import java.nio.ByteBuffer
@@ -33,6 +36,22 @@ class QRCodeAnalyzer(
                 false
             )
             val binaryBmp = BinaryBitmap(HybridBinarizer(source))
+            try {
+                val result = MultiFormatReader().apply {
+                    setHints(
+                        mapOf(
+                            DecodeHintType.POSSIBLE_FORMATS to arrayListOf(
+                                BarcodeFormat.QR_CODE
+                            )
+                        )
+                    )
+                }.decode(binaryBmp)
+                onCodeAnalyzer(result.text)
+            }catch (e : Exception){
+               e.printStackTrace()
+            }finally {
+                image.close()
+            }
         }
     }
 

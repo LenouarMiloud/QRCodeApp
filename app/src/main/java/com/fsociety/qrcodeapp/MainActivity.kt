49003f3357c616e0bdc9660bb91a3adbe7816e94
LenouarMiloud <lenouarmiloud@gmail.com>
2024-08-17 11:33:29 -0700

    terminate the app

--- app/src/main/java/com/fsociety/qrcodeapp/MainActivity.kt
+++ app/src/main/java/com/fsociety/qrcodeapp/MainActivity.kt
@@ -14,12 +14,6 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -27,9 +21,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
+import android.util.Size
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.fsociety.qrcodeapp.ui.theme.QRCodeAppTheme
@@ -60,7 +60,7 @@ fun MainScreen(){
         mutableStateOf(
             ContextCompat.checkSelfPermission(
                 context,
-                Manifest.permission.CAMERA
+                android.Manifest.permission.CAMERA
             )== PackageManager.PERMISSION_GRANTED
         )
     }
@@ -71,42 +71,55 @@ fun MainScreen(){
         }
     )
     LaunchedEffect(key1 = true) {
-        launcher.launch(Manifest.permission.CAMERA)
+        launcher.launch(android.Manifest.permission.CAMERA)
     }
     Column(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        AndroidView(factory = {context ->
-            val previewView = PreviewView(context)
-            val preview = Preview.Builder().build()
-            val selector = CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                .build()
-            preview.setSurfaceProvider(previewView.surfaceProvider)
-            val imageAnalysis = ImageAnalysis.Builder()
-                .setTargetResolution(
-                    Size(
-                        previewView.width,
-                        previewView.height
+        if(hasCameraPermission){
+
+            AndroidView(factory = {context ->
+                val previewView = PreviewView(context)
+                val preview = Preview.Builder().build()
+                val selector = CameraSelector.Builder()
+                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                    .build()
+                preview.setSurfaceProvider(previewView.surfaceProvider)
+                val imageAnalysis = ImageAnalysis.Builder()
+                    .setTargetResolution(
+                        Size(
+                            previewView.width,
+                            previewView.height
                         ))
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build()
-            imageAnalysis.setAnalyzer(
-                ContextCompat.getMainExecutor(context),
-                QRCodeAnalyzer{result->
-                    code = result
-                }
-            )
-            try{
-                cameraProviderFuture.get().bindToLifecycle(
-                    lifecycleOwner,
-                    selector,
-                    preview,
-                    imageAnalysis
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build()
+                imageAnalysis.setAnalyzer(
+                    ContextCompat.getMainExecutor(context),
+                    QRCodeAnalyzer{result->
+                        code = result
+                    }
                 )
-            }catch (e : Exception){
-                e.printStackTrace()
-            }
-        })
+                try{
+                    cameraProviderFuture.get().bindToLifecycle(
+                        lifecycleOwner,
+                        selector,
+                        preview,
+                        imageAnalysis
+                    )
+                }catch (e : Exception){
+                    e.printStackTrace()
+                }
+                previewView
+            },
+                modifier = Modifier.weight(1f)
+            )
+            Text(
+                text = code,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth()
+                    .padding(32.dp)
+                )
+        }
     }
 }
